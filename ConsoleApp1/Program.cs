d84a23f451cd1a960933af8ae2c189d164ae7d30
PtpJam <ruslan.fateev.rf@gmail.com>
2024-04-22 19:03:20 +0300

    Add for

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.BackgroundColor = ConsoleColor.White;
 Console.WriteLine("Hello, World!");
+
+Console.BackgroundColor = ConsoleColor.Blue;
+for (int i = 0; i < 43; i++) Console.WriteLine($"Hello, World! \t {i}");
+
 Console.ReadLine();
