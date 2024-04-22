bf1ab11ad6e901d20c398329a7b171c493764473
PtpJam <ruslan.fateev.rf@gmail.com>
2024-04-22 19:11:44 +0300

    Add cls

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Hello, World!\nEnter name:\t");
 string name = Console.ReadLine();
-Console.ForegroundColor = ConsoleColor.Red; 
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Clear();
 Console.WriteLine(  $"\t\t\t\tName is {name}");
 Console.ReadLine();
